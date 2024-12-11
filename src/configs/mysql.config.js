e6179e79475569e5376aab37d3f88f5d749da50a
Jinyoung Sung <sjy021621@gmail.com>
2024-12-11 22:24:05 +0900

    feat: DB 설정

--- src/configs/mysql.config.js
+++ src/configs/mysql.config.js
@@ -1,10 +1,11 @@
-// mysql.config.js
 require("dotenv").config();
 
-module.exports = {
+const dbConfig = {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT,
 };
+
+module.exports = dbConfig;
